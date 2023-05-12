abc6bbac5e3c31679c3ae4ab45b2a2906b04d0d0
Wilfred Samuel <WilfredSamuel000@gmail.com>
2023-05-12 15:20:27 +0100

    deleted:    myportfolio/build_files.sh 	modified:   myportfolio/myportfolio/__pycache__/settings.cpython-38.pyc 	modified:   myportfolio/myportfolio/__pycache__/urls.cpython-38.pyc 	modified:   myportfolio/myportfolio/__pycache__/wsgi.cpython-38.pyc 	modified:   myportfolio/myportfolio/settings.py 	modified:   myportfolio/myportfolio/urls.py 	modified:   myportfolio/myportfolio/wsgi.py 	deleted:    myportfolio/static/img/blog.png 	deleted:    myportfolio/static/img/django.jpg 	deleted:    myportfolio/static/img/python.jpg 	deleted:    myportfolio/static/img/pythonn.webp 	deleted:    myportfolio/static/img/weather.png 	deleted:    myportfolio/static/img/youtube.png 	deleted:    myportfolio/static/style.css 	deleted:    myportfolio/vercel.json

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(d+9^lqi%u7z6-2b5fp2*p*u6$xejy9^z^!*^jrs&dl$w-fq*l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
@@ -142,5 +142,3 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4" 
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
